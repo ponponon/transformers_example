4c01cdb1c7e46a312476dba78e94801b8ed5c831
ponponon <1729303158@qq.com>
2023-07-03 05:59:01 +0000

    修改模型名称

---
+++ jonatasgrosman/whisper-large-zh-cv11-demo/whisper-large-zh-cv11-demo.py
@@ -0,0 +1,33 @@
+from transformers import pipeline
+import time
+import contextlib
+
+
+@contextlib.contextmanager
+def timer(msg: str = None, logger=None):
+    if not logger:
+        from loguru import logger as loguru_logger
+        logger = loguru_logger
+
+    start = time.time()
+    yield
+    logger.debug(f'{msg}, used {round(time.time() - start,3)} s')
+
+
+with timer('加载模型'):
+    transcriber = pipeline(
+        "automatic-speech-recognition",
+        model="jonatasgrosman/whisper-large-zh-cv11"
+    )
+
+with timer('配置模型'):
+    transcriber.model.config.forced_decoder_ids = (
+        transcriber.tokenizer.get_decoder_prompt_ids(
+            language="zh",
+            task="transcribe"
+        )
+    )
+
+with timer('翻译耗时'):
+    transcription = transcriber("resource/audio/26s.wav")
+    print(transcription)
